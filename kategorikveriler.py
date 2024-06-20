e32782e808859a23fec192b546a7b80c881aadb2
anur1 <ahmettonuri@gmail.com>
2024-06-20 10:54:08 +0300

    ölçekleme

--- kategorikveriler.py
+++ kategorikveriler.py
@@ -107,6 +107,17 @@ x_train, x_test, y_train, y_test = train_test_split(s, sonuc3, test_size = 0.33,
 
 
 
+# sayısal verilerin ölçeklenmesi
+from sklearn.preprocessing import StandardScaler
+
+sc = StandardScaler()
+
+X_train = sc.fit_transform(x_train)
+X_test = sc.fit_transform(x_test) 
+
+
+
+
 
 
 
