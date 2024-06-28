ca18b1072a426d907abef899293dafc9a07e0f67
anur1 <ahmettonuri@gmail.com>
2024-06-28 11:50:56 +0300

    minor revisions

--- Bolum2/3.1 - verionislemesablonu.py
+++ Bolum2/3.1 - verionislemesablonu.py
@@ -29,14 +29,11 @@ print(satislar2)
 
 #verilerin egitim ve test icin bolunmesi
 from sklearn.model_selection import train_test_split
-
 x_train, x_test,y_train,y_test = train_test_split(aylar,satislar,test_size=0.33, random_state=0)
 
 #verilerin olceklenmesi
 from sklearn.preprocessing import StandardScaler
-
 sc=StandardScaler()
-
 X_train = sc.fit_transform(x_train)
 X_test = sc.fit_transform(x_test)
 Y_train = sc.fit_transform(y_train)
@@ -46,29 +43,22 @@ Y_test = sc.fit_transform(y_test)
 #8 - model inşası (lineer regresyon)
 from sklearn.linear_model import LinearRegression
 lr=LinearRegression()
-
 lr.fit(X_train, Y_train)
-
 tahmin = lr.predict(X_test)
 
 
 #non-standard tahmin
-
 lr.fit(x_train, y_train)
-
 tahmin2 = lr.predict(x_test)
 
 
 #veri görselleştirme
-
 #sıralama
 x_train = x_train.sort_index()
 y_train = y_train.sort_index()
-
 #çizdirme
 plt.plot(x_train, y_train)
 plt.plot(x_test, lr.predict(x_test))
-
 #etiketleme
 plt.title("aylara göre satış")
 plt.xlabel("aylar")
